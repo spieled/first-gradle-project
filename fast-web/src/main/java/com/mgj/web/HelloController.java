4c8b1e5cfc054526f4cc104487a016146434c591
yanqu <xiao_rougu@163.com>
2016-06-20 19:14:55 +0800

    添加发送和验证验证码的逻辑代码

--- fast-web/src/main/java/com/mgj/web/HelloController.java
+++ fast-web/src/main/java/com/mgj/web/HelloController.java
@@ -1,7 +1,10 @@
 package com.mgj.web;
 
+import org.apache.log4j.spi.LoggerFactory;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.slf4j.Logger;
 import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +20,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 打招呼的WEB入口
@@ -24,9 +30,41 @@ import java.util.List;
  */
 @RestController
 public class HelloController {
+
     @RequestMapping("hello")
     public String hello() {
         return "Hello Baby!";
+    }
+
+    private Map<String, String> map = new ConcurrentHashMap<>();
+    private String generateValidateCode(String phone) {
+        StringBuffer buffer = new StringBuffer();
+        Random random = new Random();
+        for (int i=0; i<6; i++) {
+            buffer.append(random.nextInt(10));
+        }
+        String code = buffer.toString();
+        map.put(phone,code);
+        return code;
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("sendValidateCode")
+    public String sendValidateCode(HttpServletRequest request, String phone) throws IOException {
+        String message = "验证码" + generateValidateCode(phone);
+        String sign = "快WIFI";
+        LuosimaoSendUtil.send(phone, message, sign);
+        return "success";
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("validateCode")
+    public String validateCode(HttpServletRequest request, String phone, String code) throws IOException {
+        String savedCode = map.get(phone);
+        if (!StringUtils.isEmpty(savedCode) && savedCode.equals(code)) {
+            return "success";
+        }
+        return "failed";
     }
 
     @CrossOrigin(origins = "*")
