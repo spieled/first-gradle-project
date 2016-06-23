5de2168d05518e3b17086b1029fac415dfe133a3
yanqu <xiao_rougu@163.com>
2016-06-23 17:03:27 +0800

    test expected exception

--- fast-core/src/main/java/com/mgj/core/UserService.java
+++ fast-core/src/main/java/com/mgj/core/UserService.java
@@ -4,6 +4,7 @@ import com.mgj.base.socialinsurance.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by yanqu on 2016/6/23.
@@ -15,6 +16,13 @@ public class UserService {
     private JdbcTemplate jdbcTemplate;
 
     public void createUser(User user) {
+        jdbcTemplate.update("INSERT INTO user (`username`, `password`) VALUES (?, ?)", new Object[] {
+                user.getUsername(), user.getPassword()
+        });
+    }
+
+    @Transactional(readOnly = true)
+    public void fakeCreateUser(User user) {
         jdbcTemplate.update("INSERT INTO user (`username`, `password`) VALUES (?, ?)", new Object[] {
                 user.getUsername(), user.getPassword()
         });
--- fast-core/src/test/java/com/mgj/core/UserServiceTest.java
+++ fast-core/src/test/java/com/mgj/core/UserServiceTest.java
@@ -2,13 +2,14 @@ package com.mgj.core;
 
 import com.mgj.base.socialinsurance.User;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by yanqu on 2016/6/23.
@@ -16,6 +17,19 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(SampleApplication.class)
 public class UserServiceTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test
+    public void fakeCreateUser() throws Exception {
+        // 测试预期异常
+        thrown.expect(TransientDataAccessResourceException.class);
+
+        User user = new User();
+        user.setUsername("root");
+        user.setPassword("root");
+        userService.fakeCreateUser(user);
+    }
+
     @Autowired
     private UserService userService;
 
