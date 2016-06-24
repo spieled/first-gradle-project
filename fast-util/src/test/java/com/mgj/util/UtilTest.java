9f58a9ae8735cd6b65b545b4ba6e60e6764e2186
yanqu <xiao_rougu@163.com>
2016-06-24 10:46:17 +0800

    import mockito

--- fast-util/src/test/java/com/mgj/util/UtilTest.java
+++ fast-util/src/test/java/com/mgj/util/UtilTest.java
@@ -1,12 +1,13 @@
 package com.mgj.util;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by yanqu on 2016/6/24.
@@ -16,9 +17,9 @@ public class UtilTest {
     @Test
     public void randomMobile() throws Exception {
         String mobile = Util.randomMobile();
-        Assert.assertNotNull("mobile should not be null", mobile);
-        Assert.assertEquals("mobile length should be 11", 11, mobile.length());
-        Assert.assertEquals("mobile should start with 1", "1", mobile.substring(0,1));
+        assertNotNull("mobile should not be null", mobile);
+        assertEquals("mobile length should be 11", 11, mobile.length());
+        assertEquals("mobile should start with 1", "1", mobile.substring(0,1));
 
     }
 
@@ -29,6 +30,16 @@ public class UtilTest {
 
     @Test
     public void getIpAddr() throws Exception {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+
+        final String ip = "192.168.2.2";
+        when(request.getHeader("x-forwarded-for")).thenReturn(ip);
+        when(request.getHeader("Proxy-Client-IP")).thenReturn(ip);
+        when(request.getHeader("WL-Proxy-Client-IP")).thenReturn(ip);
+
+        String ipAddr = Util.getIpAddr(request);
+        assertNotNull(ipAddr);
+        assertEquals("ip should be " + ip, ip, ipAddr);
 
     }
 
